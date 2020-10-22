de7bcade128753a4e4d2f9567e0d08cd80cccb04
tmdals9974 <tmdals9974@naver.com>
2020-10-23 01:15:53 +0900

    프로젝트 생성 및 메인페이지 완성

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var favicon = require('serve-favicon')
+
+app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname +  '/public/images/favicon.ico'));
+app.set('view engine', 'ejs');
+
+app.listen(3000, () => { console.log("http://localhost:3000")});
+
+app.all('/', (req, res) => {
+    res.render(path.join(__dirname, "./views/main.ejs"), {isLogin : false, hasProject : false});
+});
+
+app.all('/', (req, res) => {
+    res.render(path.join(__dirname, "./views/login.ejs"));
+})
